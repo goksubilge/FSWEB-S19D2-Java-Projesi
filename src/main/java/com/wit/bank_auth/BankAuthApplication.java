46decc99cb3abfc546b0d43d974b083d1f79f05f
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-07-29 14:36:37 +0300

    init project, create SQL schema+tables, add dependencies

---
+++ src/main/java/com/wit/bank_auth/BankAuthApplication.java
@@ -0,0 +1,13 @@
+package com.wit.bank_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wit/bank_auth/BankAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wit.bank_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
